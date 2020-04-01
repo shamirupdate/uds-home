2005c29da00e56dc6e9362bd857e4112ced50a0d
shamir mohamed <shamirmohamed@shamirs-Mac-Pro.local>
2020-04-01 23:52:53 +0530

    disabled heading

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,9 +17,9 @@ export default () => (
     <section className="pt-20 md:pt-40 landing-section">
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
-          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
+          {/* <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
             #Updatestudios - [social]
-          </h1>
+          </h1> */}
           <p className="text-xl lg:text-2xl mt-6 font-light">
           Update Studios is an independent pre-production, production and post-production studio that specializes in video editing, 
           color grading, dubbing, Foley recording, chroma key green room studio with standard lights and equipments & preview theatre 
