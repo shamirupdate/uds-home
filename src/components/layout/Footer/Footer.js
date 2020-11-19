067961db016cf08872568e7ca27d6d898f8223cc
Santhosh Soundararajan <Santhosh@Santhoshs-MacBook-Pro.local>
2020-11-19 12:00:40 -0800

    updated footer color and icons

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { FaInstagram, FaTwitter, FaFacebookSquare } from 'react-icons/fa';
+import { FaInstagram, FaTwitter, FaFacebookSquare, FaPhoneAlt } from 'react-icons/fa';
 import { LogoIcon } from '../../../svg/LogoIcon';
+import { SiGmail } from 'react-icons/si';
 import './Footer.css';
 
 const Footer = () => (
@@ -19,24 +20,29 @@ const Footer = () => (
         </div>
         <div className="flex-1 px-3">
           <h2 className="text-lg font-semibold">Social Media</h2>
-          <ul className="mt-2 leading-loose mb-4">
+          <ul className="mt-2 leading-loose mb-4 media-link">
             <li>
-              <a className="social-link" href="https://www.instagram.com/updatestudios/"><FaInstagram /> #updatestudios</a>
+              <a className="icons social-link" href="https://www.instagram.com/updatestudios/" 
+                target="_blank"><FaInstagram class="instaIcon"/> #updatestudios</a>
             </li>
             <li>
-              <a className="social-link" href="https://www.facebook.com/updatestudios/"><FaFacebookSquare /> updatestudios</a>
+              <a className="icons social-link" href="https://www.facebook.com/updatestudios/"
+                target="_blank"><FaFacebookSquare class="fbIcon"/> updatestudios</a>
             </li>
             <li>
-              <a className="social-link" href="https://twitter.com/Shamir_uds"><FaTwitter /> @Shamir_uds</a>
+              <a className="icons social-link" href="https://twitter.com/Shamir_uds"
+                target="_blank"><FaTwitter class="twitterIcon"/> @Shamir_uds</a>
             </li>
           </ul>
           <h2 className="text-lg font-semibold">Contact</h2>
           <ul className="mt-2 leading-loose">
             <li>
-              Mobile: 9940760786
+              <a href="tel:9940-760-786" class="icons mobile"
+                target="_blank"><FaPhoneAlt class="mobileIcon"/> 9940-760-786</a>
             </li>
             <li>
-              <a href="mailto:shamir@updatestudios.com" rel="noopener" class="icons" aria-label="Mail">shamir@updatestudios.com</a>
+              <a href="mailto:shamir@updatestudios.com" rel="noopener" class="icons email" aria-label="Mail"
+                target="_blank"><SiGmail class="emailIcon"/> shamir@updatestudios.com</a>
             </li>
           </ul>
         </div>
